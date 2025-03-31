4958f0bb42ca61cbfa424fb450605854cfbfd335
Riko <riko.fluchel@gmail.com>
2025-03-31 07:36:10 -0400

    add initial fetch and render of pokemon

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,12 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokemon}) {
+  //const [id, name, hp, sprites] = pokemon;
+
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      {pokemon.map((item) => <PokemonCard key={item.id} id={item.id} name={item.name} hp={item.hp} sprites={item.sprites} />)}
     </Card.Group>
   );
 }
